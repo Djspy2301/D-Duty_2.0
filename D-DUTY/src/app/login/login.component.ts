b28a5b30473d1a436601fedc70c53225112e6940
Adit <arnavle1998@gmail.com>
2023-10-02 19:17:19 +0530

    pageReload fix

--- D-DUTY/src/app/login/login.component.ts
+++ D-DUTY/src/app/login/login.component.ts
@@ -24,34 +24,6 @@ export class LoginComponent {
     password: this.builder.control('', Validators.required),
   });
 
-  // proceedLogin() {
-  //   if (this.loginForm.valid) {
-  //     this.authService.getByCode(this.loginForm.value.id).subscribe((res) => {
-  //       this.userData = res;
-  //       console.log(this.userData);
-  //       if (this.userData.password === this.loginForm.value.password) {
-  //         sessionStorage.setItem('username', this.userData.id);
-  //         sessionStorage.setItem('name', this.userData.name);
-  //         sessionStorage.setItem('role', this.userData.role);
-  //         sessionStorage.setItem('org', this.userData.org);
-  //         const logId = sessionStorage.getItem('username');
-  //         console.log(logId);
-  //         if (this.userData.role === 'Admin') {
-  //           this.toast.success('Login Successfully!');
-  //           this.route.navigate(['admin', logId, 'dashboard']);
-  //         } else if (this.userData.role === 'User') {
-  //           this.toast.success('Login Successfully!');
-  //           this.route.navigate(['user', logId, 'dashboard']);
-  //         }
-  //       } else {
-  //         this.toast.warning('Invalid credientials!');
-  //       }
-  //     });
-  //   } else {
-  //     this.toast.warning('Input credientials!');
-  //   }
-  // }
-
   proceedLogin() {
     if (this.loginForm.valid) {
       this.authService.getByCode(this.loginForm.value).subscribe(
@@ -63,13 +35,17 @@ export class LoginComponent {
             sessionStorage.setItem('name', res.name);
             sessionStorage.setItem('role', res.role);
             sessionStorage.setItem('org', res.org);
-            this.route.navigate(['admin', id, 'dashboard']);
+            this.route.navigate(['admin', id, 'dashboard']).then(() => {
+              window.location.reload();
+            });
           } else if (res.role === 'User') {
             sessionStorage.setItem('user', res.user);
             sessionStorage.setItem('name', res.name);
             sessionStorage.setItem('role', res.role);
             sessionStorage.setItem('regBy', res.regBy);
-            this.route.navigate(['user', id, 'dashboard']);
+            this.route.navigate(['user', id, 'dashboard']).then(() => {
+              window.location.reload();
+            });
           }
         },
         (error: any) => {
